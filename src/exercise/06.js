1eb57d0ec4bdd55b905da276bfcc630ffcf8cbca
PavanKruthik <kruthikpavan24@gmail.com>
2022-07-23 22:48:09 +0530

    solved excercises

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,27 +4,43 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const [username, setUserName] = React.useState('')
+  const [errorMessage, setErrorMessage] = React.useState(null)
+  const userName = React.useRef(null)
+  function handleSubmitForm(event) {
+    event.preventDefault()
+    // onSubmitUsername('Hi ' + ' ' + event.target.elements.usernameInput.value)
+    // onSubmitUsername(userName.current.value)
+    onSubmitUsername(username)
+  }
 
-  // 🐨 add the onSubmit handler to the <form> below
+  function handleInputChange(event) {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    isLowerCase
+      ? setErrorMessage(null)
+      : setErrorMessage('Only lowercase letters are allowed')
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+    setUserName(value.toLowerCase())
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={e => {
+        handleSubmitForm(e)
+      }}
+    >
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          onChange={handleInputChange}
+          ref={userName}
+          value={username}
+          id="usernameInput"
+          type="text"
+        />
       </div>
+      {/* <div style={{color: 'red'}}>{errorMessage}</div> */}
       <button type="submit">Submit</button>
     </form>
   )
